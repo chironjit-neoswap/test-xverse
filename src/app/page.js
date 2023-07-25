8b2e9e4b340bd0db81ecd5588774b052c0a8a80c
Chironjit Das <chironjit.das@neoswap.ai>
2023-07-25 19:48:17 +0200

    Test xverse pubkey validation

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,90 @@
-import Image from 'next/image'
+"use client"
+import React, { useState } from "react";
+import { getAddress, signTransaction, signMessage  } from "sats-connect";
+import * as btc from '@scure/btc-signer';
+import { hex } from '@scure/base';
 
 export default function Home() {
+  
+  const [state, setState] = useState({
+    paymentAddress: "",
+    paymentPubKey: "",
+    ordinalsAddress: "",
+    ordinalsPubKey: "",
+  });
+
+  const [isValidOrdinals, setIsValidOrdinals] = useState(false);
+  const [isValidPayment, setIsValidPayment] = useState(false);
+
+  const onConnectClick = async () => {
+    const getAddressOptions = {
+      payload: {
+        purposes: ['ordinals', 'payment'],
+        message: 'Address for receiving Ordinals and payments',
+        network: {
+          type:'Mainnet'
+        },
+      },
+      onFinish: (response) => {
+        console.log(response)
+        setState({
+          ordinalsAddress: response.addresses[0].address,
+          ordinalsPubKey: response.addresses[0].publicKey,
+          paymentAddress: response.addresses[1].address,
+          paymentPubKey: response.addresses[1].publicKey,
+        });
+        validatePubKey({addresses: response.addresses});
+      },
+      onCancel: () => alert('Request canceled'),
+      }
+        
+    await getAddress(getAddressOptions);
+    
+  };
+
+  const validatePubKey = async({addresses}) => {
+    const network = btc.NETWORK;
+
+    console.log('addresses', addresses);
+
+    // format ordinals (taproot) address data
+    const userInternalPubKey = hex.decode(addresses[0].publicKey);
+    const ordinals = btc.p2tr(userInternalPubKey, undefined, network);
+
+    // format payment (wrapped-segwit) address data
+    const userPubKey = hex.decode(addresses[1].publicKey);
+    const userP2wpkh = btc.p2wpkh(userPubKey, network);
+    const payment = btc.p2sh(userP2wpkh, network);
+
+  
+    if(ordinals.address === addresses[0].address) setIsValidOrdinals(true);
+    if(payment.address === addresses[1].address) setIsValidPayment(true);
+    
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main className="flex min-h-screen items-center justify-center p-24 flex-col">
+      <div className="mb-4">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={onConnectClick}>
+          Connect
+        </button>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Show wallet info
+        </button>
       </div>
+      <div>
+        <br />
+        <div>Payment Address: {state.paymentAddress}</div>
+        <div>Payment Public Key: {state.paymentPubKey}</div>
+        <div>Address matches pubkey: {`${isValidPayment}`}</div>
+        <br />
+        <div>Ordinals Address: {state.ordinalsAddress}</div>
+        <div>Ordinals Public Key: {state.ordinalsPubKey}</div>
+        <div>Address matches pubkey: {`${isValidOrdinals}`}</div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
-  )
+  );
 }
